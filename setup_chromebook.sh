47569934aac1a2f023cc5b2fa0d5bd3eb3f514ae
Maish Saidel-Keesing <maishsk@gmail.com>
2018-08-27 11:33:38 +0000

    removed specific version for hub

--- setup_chromebook.sh
+++ setup_chromebook.sh
@@ -35,12 +35,12 @@ sudo mv terraform /usr/local/bin
 rm -rf terraform_0.11.7_linux_arm.zip
 
 #Install hub
-wget https://github.com/github/hub/releases/download/v2.4.0/hub-linux-arm64-2.4.0.tgz
-tar zvxf hub-linux-arm64-2.5.0.tgz
-sudo mv hub-linux-arm64-2.5.0/bin/hub /usr/local/bin/
-mv hub-linux-arm64-2.5.0/etc/hub.bash_completion.sh ~/
+wget https://github.com/github/hub/releases/download/v2.5.1/hub-linux-arm64-2.5.1.tgz
+tar zvxf hub-linux-arm64-*
+sudo mv hub-linux-arm64-2.*/bin/hub /usr/local/bin/
+mv hub-linux-arm64-*/etc/hub.bash_completion.sh ~/
 echo ". $HOME/hub.bash_completion.sh" >> ~/.bash_profile
-rm -rf hub-linux-arm64-2.4.0*
+rm -rf hub-linux-**
 echo 'eval "$(hub alias -s)"' >> ~/.bash_profile
 
 #Install pip
@@ -52,6 +52,5 @@ rm -rf get-pip.py
 pip install --user --upgrade awscli ansible boto boto3
 
 ##Personalize
-echo
 
 #compile aws-vault
