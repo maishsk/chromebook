eca25ce55698bf36235a897d836161671743207a
Maish <maishsk@users.noreply.github.com>
2018-12-24 11:51:32 +0200

    Added get latest version from github (#3)
    - Added get latest version script from github
    - set scripts to use variables for interactions with versions

--- setup_chromebook.sh
+++ setup_chromebook.sh
@@ -1,37 +1,47 @@
 #!/bin/bash
 
-## Outside chroot
-#Setup crouton chroot
-cd ~
-sudo ln -s /media/removable/SD\ Card/ sdcard
+# ## Outside chroot
+# #Setup crouton chroot
+# cd ~
+# sudo ln -s /media/removable/SD\ Card/ sdcard
 
-cd ~/Downloads
-sudo sh ~/Downloads/crouton -d -t core,cli-extra,touch,xfce -f ~/Downloads/mybootstrap.tar.bz2
-sudo sh ~/Downloads/crouton -t core,cli-extra,touch -a armhf -f ~/Downloads/mybootstrap.tar.bz2 -n xenial
+# cd ~/Downloads
+# sudo sh ~/Downloads/crouton -d -t core,cli-extra,touch,xfce -f ~/Downloads/mybootstrap.tar.bz2
+# sudo sh ~/Downloads/crouton -t core,cli-extra,touch -a armhf -f ~/Downloads/mybootstrap.tar.bz2 -n xenial
 
-#Inside chroot
+# #Inside chroot
 
-sudo ln -s /media/removable/SD\ Card/movies /movies
-sudo ln -s /media/removable/SD\ Card/git /opt/git
-cp -r /opt/git/private/.bash* /opt/git/private/.gitconfig /opt/git/private/.aws /opt/git/private/.ssh  ~/
-chmod 400 ~/.ssh/id_rsa
+# sudo ln -s /media/removable/SD\ Card/movies /movies
+# sudo ln -s /media/removable/SD\ Card/git /opt/git
+# cp -r /opt/git/private/.bash* /opt/git/private/.gitconfig /opt/git/private/.aws /opt/git/private/.ssh  ~/
+# chmod 400 ~/.ssh/id_rsa
 
+# Get latest release function
+get_latest() {
+    curl -s "https://api.github.com/repos/$1/releases/latest" |
+      grep '"tag_name":' |
+        sed -E 's/.*"([^"]+)".*/\1/'
+}
 
-#Install Terraform
+#Install base packages
 sudo apt-get install -y unzip libffi-dev libssl-dev git curl wget python gcc vim groff build-essential dnsutils python-dev libxml2-dev libxslt1-dev
-wget https://releases.hashicorp.com/terraform/0.11.7/terraform_0.11.7_linux_arm.zip
-unzip terraform_0.11.7_linux_arm.zip
+
+tf_version=$(get_latest hashicorp/terraform | cut -c 2-)
+#Install Terraform
+wget https://releases.hashicorp.com/terraform/${tf_version}/terraform_${tf_version}_linux_arm.zip
+unzip terraform_${tf_version}_linux_arm.zip
 chmod +x terraform
 sudo mv terraform /usr/local/bin
-rm -rf terraform_0.11.7_linux_arm.zip
+rm -rf terraform_${tf_version}_linux_arm.zip
 
 #Install hub
-wget https://github.com/github/hub/releases/download/v2.5.1/hub-linux-arm64-2.5.1.tgz
-tar zvxf hub-linux-arm64-*
-sudo mv hub-linux-arm64-2.*/bin/hub /usr/local/bin/
-mv hub-linux-arm64-*/etc/hub.bash_completion.sh ~/
+hub_version=$(get_latest github/hub | cut -c 2-)
+wget https://github.com/github/hub/releases/download/v${hub_version}/hub-linux-arm64-${hub_version}.tgz
+tar zvxf hub-linux-arm64-${hub_version}.tgz
+sudo mv hub-linux-arm64-${hub_version}/bin/hub /usr/local/bin/
+mv hub-linux-arm64-${hub_version}/etc/hub.bash_completion.sh ~/
 echo ". $HOME/hub.bash_completion.sh" >> ~/.bash_profile
-rm -rf hub-linux-**
+rm -rf hub-linux-${hub_version}
 echo 'eval "$(hub alias -s)"' >> ~/.bash_profile
 
 #Install pip
